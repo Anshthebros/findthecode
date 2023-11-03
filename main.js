2c08106b7b6e84839cfd2ce7a7825d21db52d12c
Anshthebros <145959854+Anshthebros@users.noreply.github.com>
2023-11-03 17:19:08 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function a(){
+    var name=document.getElementById("g").value;
+    if(name=="games"){
+        window.open("https://anshthebros.github.io/games/");
+    }
+
+    if(name=="tim berners lee"){
+        window.open("https://anshthebros.github.io/tim-berners-lee/")
+    }
+
+    if(name=="blog"){
+        window.open("https://anshthebros.github.io/blog/")
+    }
+}
